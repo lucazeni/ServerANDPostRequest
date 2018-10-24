2098267fe54d9144a438d0a1d65353162b731c17
Luca Zeni <luca_zeni@hotmail.com>
2018-10-24 16:59:16 -0400

    HTTPPOST_JSON

--- postMessageJSON.cs
+++ postMessageJSON.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,27 +8,41 @@ public class postMessageJSON : MonoBehaviour {
     // upload JSON to HTTP server //
     void Start()
     {
-        StartCoroutine(PostRequest("http://192.168.0.11:8080/", "your json")); // send json to IP address @ port 8080 
+        StartCoroutine(PostRequest("http://192.168.0.23:8080/","Luca_Zeni", "Test1234", "Hello My Name Is Luca Zeni And I Attend York Univeristy"));  // example on how to use method
     }
 
-    IEnumerator PostRequest(string url, string json) // URL + JSON
+    IEnumerator PostRequest(string url, string id, string fileName, string json)
     {
-        var uwr = new UnityWebRequest(url, "POST"); // type of HTTPREQUEST
-        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json); // encode JSON
-        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend); // upload JSON
-        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer(); 
-        uwr.SetRequestHeader("Content-Type", "application/json"); // set type
+        if(id.Contains(" "))
+        {
+            throw new Exception("No spaces allowed in id name");
+        }
+        if (fileName.Contains(" "))
+        {
+            throw new Exception("No spaces allowed in file name");
+        }
+        else if(fileName.Equals("") || fileName == null)
+        {
+            throw new Exception("File name null");
+        }
+
+        var request = new UnityWebRequest(url, "POST"); // type of HTTPREQUEST POST in this case
+        byte[] infoToSend = new System.Text.UTF8Encoding().GetBytes(id + " " + fileName + " " + json); 
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(infoToSend); 
+        
+        request.SetRequestHeader("Content-Type", "application/json"); 
 
         //Send the request then wait here until it returns
-        yield return uwr.SendWebRequest();
+        yield return request.SendWebRequest();
 
-        if (uwr.isNetworkError)
+        if (request.isNetworkError)
         {
-            Debug.Log("Error While Sending: " + uwr.error);
+            Debug.Log("Error While Sending: " + request.error);
         }
         else
         {
-            Debug.Log("Received: " + uwr.downloadHandler.text);
+            Debug.Log("Received");
         }
     }
 
